27c7ceddb548d6f68912bbf5248f7873670f9981
Zach Comer <zachjcomer@gmail.com>
2025-06-07 13:29:17 -0400

    Add GitHub pages pipeline take 1

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
     react(),
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+      process.env.REPL_ID !== undefined
       ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
+        await import("@replit/vite-plugin-cartographer").then((m) =>
+          m.cartographer(),
+        ),
+      ]
       : []),
   ],
   resolve: {
@@ -34,4 +34,5 @@ export default defineConfig({
       deny: ["**/.*"],
     },
   },
+  base: '/Flagship-Site/'
 });
